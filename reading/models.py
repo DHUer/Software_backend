406e174a170b18038968f4f9c70ba5ab54e03dc6
AlexhahDeng <751925352@qq.com>
2019-05-13 09:01:58 +0800

    更新后端
    news 是django的app
    tutorial里面放的是scrapy
    res是爬取的文章信息

--- reading/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class User(object):
-    username = models.CharField(max_length = 20)
-    mailbox = models.CharField(max_length = 20)
-    password = models.CharField(max_length = 20)
-
